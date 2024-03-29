1a63bb38d4294ed0700d6b1d5ac331ad478dc6bf
Ahad0909 <159162858+Ahad0909@users.noreply.github.com>
2024-03-30 02:58:42 +0500

    CLI number guesss game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 6 + 1)
+
+const answers = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number between 1 to 6:"
+}])
+ 
+if(answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you have guesssed right number")
+}
+else {
+    console.log("Wrong! your guess is incorrect")
+}
+
+console.log("THE END")
